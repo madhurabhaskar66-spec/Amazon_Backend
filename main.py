72ba177c2f1edc31d7c2a739af3b9df25ae66f5c
Madhura B <madhurabhaskar66@gmail.com>
2026-02-13 13:17:55 +0530

    push

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from db import get_db,DATABASE_URL
 from sqlalchemy import create_engine
-import os
 from models import Base
+import os
 app = FastAPI()
 
 #cors
